9574050ba7adee6daa105ce6e82052e051bf2ff3
Anwesh43 <anweshthecool0@gmail.com>
2018-09-23 01:35:01 +0530

    importing libraries and defining nodes

--- xsteptolineview/src/main/java/com/anwesh/uiprojects/xsteptolineview/XStepToLineView.kt
+++ xsteptolineview/src/main/java/com/anwesh/uiprojects/xsteptolineview/XStepToLineView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.xsteptolineview
 /**
  * Created by anweshmishra on 23/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.content.Context
+import android.graphics.Color
+
+val nodes : Int = 5
+
