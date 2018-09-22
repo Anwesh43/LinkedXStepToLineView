124ee351139c1e4908f1d7dc09a69d19b29cb891
Anwesh43 <anweshthecool0@gmail.com>
2018-09-23 01:37:25 +0530

    adding XStepToLineView class

--- xsteptolineview/src/main/java/com/anwesh/uiprojects/xsteptolineview/XStepToLineView.kt
+++ xsteptolineview/src/main/java/com/anwesh/uiprojects/xsteptolineview/XStepToLineView.kt
@@ -13,3 +13,20 @@ import android.graphics.Color
 
 val nodes : Int = 5
 
+class XStepToLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
