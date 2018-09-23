ef8957d1412b0e78c8a8ff2fa2450e97c6fc09a4
Anwesh43 <anweshthecool0@gmail.com>
2018-09-23 08:55:25 +0530

    adding XStepToLine class

--- xsteptolineview/src/main/java/com/anwesh/uiprojects/xsteptolineview/XStepToLineView.kt
+++ xsteptolineview/src/main/java/com/anwesh/uiprojects/xsteptolineview/XStepToLineView.kt
@@ -141,4 +141,26 @@ class XStepToLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class XStepToLine(var i : Int) {
+        private var curr : XSTLNode = XSTLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
