6dbbce4b2e469cd729ee766e991f64b7328f7b6d
Anwesh43 <anweshthecool0@gmail.com>
2018-09-23 08:43:45 +0530

    adding drawXSTLNode canvas extension method

--- xsteptolineview/src/main/java/com/anwesh/uiprojects/xsteptolineview/XStepToLineView.kt
+++ xsteptolineview/src/main/java/com/anwesh/uiprojects/xsteptolineview/XStepToLineView.kt
@@ -13,6 +13,27 @@ import android.graphics.Color
 
 val nodes : Int = 5
 
+fun Canvas.drawXSTLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / 3
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = Color.parseColor("#ef5350")
+    save()
+    translate(w/2, gap + i * gap)
+    for (j in 0..1) {
+        val sf : Float = 1f - 2 * (j % 2)
+        val sc : Float = Math.min(0.5f, Math.max(0f, scale - j * 0.5f)) * 2
+        save()
+        rotate(45f * sf * (1 - sc))
+        drawLine(0f, -size, 0f, size, paint)
+        restore()
+    }
+    restore()
+}
+
 class XStepToLineView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -32,7 +53,7 @@ class XStepToLineView(ctx : Context) : View(ctx) {
 
     data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
         fun update(cb : (Float) -> Unit) {
-            scale += 0.1f * dir
+            scale += 0.05f * dir
             if (Math.abs(scale - prevScale) > 1) {
                 scale = prevScale + dir
                 dir = 0f
